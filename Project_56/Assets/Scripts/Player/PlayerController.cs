51b657cca8db4a148b4f0f403a8e69a15e5c2214
Dzolver <kaitoano0@gmail.com>
2018-10-08 03:05:52 +0530

    Commit#1 - Organized File Structure with added Sample Resources

---
+++ Project_56/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float moveSpeed;
+	public float jumpForce;
+
+	private Rigidbody2D RunnerRigidBody;
+
+	// Use this for initialization
+	void Start () {
+		//initialize the component variables by searching for all needed components using GetComponent
+		RunnerRigidBody = GetComponent<Rigidbody2D> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Character will move in a direction with each frame
+		RunnerRigidBody.velocity = new Vector2 (moveSpeed, RunnerRigidBody.velocity.y);
+
+		if(Input.GetKeyDown(KeyCode.Space) )
+		{
+			RunnerRigidBody.velocity = new Vector2 (RunnerRigidBody.velocity.x, jumpForce);
+		}
+	}
+}
