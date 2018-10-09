968275e69be6a11c9c266f04b9fe08f499fceae3
Dzolver <kaitoano0@gmail.com>
2018-10-10 01:13:13 +0530

    Added player run/jump animations and customized camera to follow

---
+++ Project_56/Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public PlayerController theRunner;
+
+	private Vector3 lastRunnerPosition;
+	private float distanceToMove;
+
+	// Use this for initialization
+	void Start () {
+		//Finding reference to the player 
+		theRunner = FindObjectOfType<PlayerController>();
+		//Initialize the last player position for the first frame
+		lastRunnerPosition = theRunner.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Calculate the distance to move the camera
+		distanceToMove = theRunner.transform.position.x - lastRunnerPosition.x;
+
+		transform.position = new Vector3 (transform.position.x + distanceToMove, transform.position.y, transform.position.z); 
+		//updating the last player position every frame
+		lastRunnerPosition = theRunner.transform.position;
+
+	}
+}
--- Project_56/Assets/Scripts/Player/PlayerController.cs
+++ Project_56/Assets/Scripts/Player/PlayerController.cs
@@ -3,25 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+	public bool grounded;
+	public LayerMask whatIsGround;
 	public float moveSpeed;
 	public float jumpForce;
 
+	private Collider2D RunnerCollider;
 	private Rigidbody2D RunnerRigidBody;
+	private Animator RunnerAnimator;
 
 	// Use this for initialization
 	void Start () {
 		//initialize the component variables by searching for all needed components using GetComponent
+		RunnerCollider = GetComponent<Collider2D>();
 		RunnerRigidBody = GetComponent<Rigidbody2D> ();	
+		RunnerAnimator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//returns true or false whether the collider is touching another collider containing the layer called 'Ground'
+		grounded = Physics2D.IsTouchingLayers (RunnerCollider, whatIsGround);
 		//Character will move in a direction with each frame
 		RunnerRigidBody.velocity = new Vector2 (moveSpeed, RunnerRigidBody.velocity.y);
 
-		if(Input.GetKeyDown(KeyCode.Space) )
+		if(Input.GetKeyDown(KeyCode.Space) && grounded == true)
 		{
 			RunnerRigidBody.velocity = new Vector2 (RunnerRigidBody.velocity.x, jumpForce);
 		}
+
+		RunnerAnimator.SetFloat ("Speed", RunnerRigidBody.velocity.x);
+		RunnerAnimator.SetBool ("Grounded", grounded);
 	}
 }
