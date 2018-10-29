c039d8f0beab98c851fb90bb26fd52a6ea18ecfe
Balpreet Singh <devknightowl@gmail.com>
2018-10-29 15:18:41 +0530

    -- Player Controller

--- Project_56/Assets/Scripts/Player/PlayerController.cs
+++ Project_56/Assets/Scripts/Player/PlayerController.cs
@@ -15,6 +15,12 @@ public class PlayerController : MonoBehaviour
     private Collider2D RunnerCollider;
     private Rigidbody2D RunnerRigidBody;
     private Animator RunnerAnimator;
+    private Vector2 m_FirstPressPos;
+    private Vector2 m_SecondPressPos;
+    private Vector2 m_CurrentSwipe;
+    private int m_MoveDirection;
+
+    public float SwipeDetectionSensitivity;
 
     // Use this for initialization
     private void Start()
@@ -28,6 +34,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        MouseSwipe();
+        TouchSwipe();
         //returns true or false whether the collider is touching another collider containing the layer called 'Ground'
         grounded = Physics2D.IsTouchingLayers(RunnerCollider, whatIsGround);
         //Character will move in a direction with each frame
@@ -40,5 +48,60 @@ public class PlayerController : MonoBehaviour
 
         RunnerAnimator.SetFloat("Speed", RunnerRigidBody.velocity.x);
         RunnerAnimator.SetBool("Grounded", grounded);
+    }
+
+    private void MouseSwipe()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            m_FirstPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+        if (Input.GetMouseButton(0) || Input.GetMouseButtonUp(0))
+        {
+            m_SecondPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            MovePlayer();
+        }
+    }
+
+    private void TouchSwipe()
+    {
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                m_FirstPressPos = touch.position;
+            }
+            if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Ended)
+            {
+                m_SecondPressPos = touch.position;
+                MovePlayer();
+                m_FirstPressPos = touch.position;
+            }
+        }
+    }
+
+    public void MovePlayer()
+    {
+        //create vector from the two points
+        m_CurrentSwipe = new Vector2(m_SecondPressPos.x - m_FirstPressPos.x, m_SecondPressPos.y - m_FirstPressPos.y);
+
+        if (Mathf.Abs(m_CurrentSwipe.x) <= SwipeDetectionSensitivity)
+        {
+            return;
+        }
+
+        //normalize the 2d vector
+        m_CurrentSwipe.Normalize();
+
+        if (m_CurrentSwipe.x < 0)
+        {
+            RunnerRigidBody.velocity = Vector2.zero;
+            moveSpeed = -Mathf.Abs(moveSpeed);
+        }
+        else if (m_CurrentSwipe.x > 0/* && (currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)*/)
+        {
+            moveSpeed = Mathf.Abs(moveSpeed);
+        }
     }
 }
