61640a7ad95497e44f6468c7079e3d987ff1857d
Balpreet Singh <devknightowl@gmail.com>
2019-03-04 18:01:23 +0530

    -- Implemented Parralax with 2 layers ( need to implement a fix for direction change) -- change platform with new asset for now

---
+++ Project_56/Assets/Scripts/UI/Items/Mybackground.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Mybackground : MonoBehaviour
+{
+    public BackgroundType backgroundType;
+    // Use this for initialization
+
+}
+
+public enum BackgroundType
+{
+    Type1,
+    Type2
+}
---
+++ Project_56/Assets/Scripts/UI/ParralaxBackground.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class ParralaxBackground : MonoBehaviour
+{
+
+    //public float speed = 1.0f;
+    //Renderer myRenderer;
+    //Vector2 offset = Vector2.zero;
+    //private Vector2 savedOffset;
+
+    //private void Start()
+    //{
+    //    myRenderer = GetComponent<Renderer>();
+    //    savedOffset = myRenderer.sharedMaterial.GetTextureOffset("_MainTex");
+    //}
+
+    //private void Update()
+    //{
+    //    float x = Mathf.Repeat(Time.time * speed, 1);
+    //    Vector2 offset = new Vector2(x, savedOffset.y);
+    //    Debug.Log("Offset-" + x);
+    //    myRenderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+    //}
+    public float scrollSpeed;
+    public float tileSizeZ;
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+        transform.position = startPosition + (-Vector3.right) * newPosition;
+    }
+}
