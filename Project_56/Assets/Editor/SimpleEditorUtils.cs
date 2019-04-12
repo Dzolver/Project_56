680220da633a4ad9e1b79d4c4805fecabfefe81f
Balpreet Singh <devknightowl@gmail.com>
2019-04-12 16:00:08 +0530

    Changes in parallax

---
+++ Project_56/Assets/Editor/SimpleEditorUtils.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public static class SimpleEditorUtils
+{
+    // click Ctrl+Q to go to the prelaunch scene and then play
+    //% - ctrl on Windows, cmd on macOS
+    //# - shift
+    //& - alt
+    [MenuItem("Edit/Play From Any Scene-Unplay  %Q")]
+    public static void PlayFromPrelaunchScene()
+    {
+        if (EditorApplication.isPlaying == true)
+        {
+            EditorApplication.isPlaying = false;
+            return;
+        }
+        EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        EditorSceneManager.OpenScene("Assets/Scenes/Splash.unity");
+        EditorApplication.isPlaying = true;
+    }
+}
