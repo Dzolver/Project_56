3b1dde8cbaeda2bb18b44cdfbb7acb14ebb1cfb0
Balpreet Singh <devknightowl@gmail.com>
2019-06-14 11:44:02 +0530

    -- Player File Added Camera shake

--- Alyx-Adventure/Assets/Scripts/Player/Player.cs
+++ Alyx-Adventure/Assets/Scripts/Player/Player.cs
@@ -52,7 +52,7 @@ namespace AlyxAdventure
             {
                 if (collision.gameObject.CompareTag(GameStrings.Enemy))
                 {
-                    CameraShake.Instance.shakeDuration = 1f;
+                    CameraShake.Instance.shakeDuration = .2f;
                     ScoreManager.Instance.AddKills();
                     collision.gameObject.GetComponent<AbstractEnemy>().Deactivate();
 
