da5f3262c64db15f4e448dee97da2bc738fab5e6
Gaming Dev <devknightowl@gmail.com>
2019-09-10 12:01:43 +0530

    Parallax speed adjustment

--- Alyx-Adventure/Assets/Scripts/UI/ParralaxBackground.cs
+++ Alyx-Adventure/Assets/Scripts/UI/ParralaxBackground.cs
@@ -5,16 +5,12 @@ namespace AlyxAdventure
 
     public class ParralaxBackground : MonoBehaviour
     {
-        //public GameObject CurrentSprite;
-        //public GameObject LeftSprite;
-        //public GameObject RightSprite;
 
         public float scrollSpeedFactor;
+        private float factor;
 
-        //private Vector3 startPosition;
-        //private Renderer myRenderer;
         private Direction direction;
-        Renderer mRenderer;
+        private Renderer mRenderer;
 
         private void Awake()
         {
@@ -34,72 +30,26 @@ namespace AlyxAdventure
             }
         }
 
+        private void ChangeMoveDirection(Direction direction)
+        {
+            this.direction = direction;
+        }
+
         void Start()
         {
             mRenderer.material.mainTextureOffset = Vector2.zero;
+            factor = GameData.Instance.theRunner.GetComponent<PlayerController>().moveSpeed / scrollSpeedFactor;
             StartCoroutine(ChangeOffset());
-            //LeftSprite.GetComponent<Renderer>().material.mainTextureOffset = Vector2.zero;
-            //RightSprite.GetComponent<Renderer>().material.mainTextureOffset = Vector2.zero;
         }
 
         private IEnumerator ChangeOffset()
         {
             while (true)
             {
-                Debug.Log("changing - " + name);
-                mRenderer.material.mainTextureOffset = mRenderer.material.mainTextureOffset + new Vector2((int)direction * scrollSpeedFactor, 0f);
+                float x = GameData.Instance.RunnerVelocity.x / factor;
+                mRenderer.material.mainTextureOffset = mRenderer.material.mainTextureOffset + new Vector2(/*(int)direction **/x, 0f);
                 yield return 0.1f;
             }
-        }
-
-        //void Update()
-        //{
-        //    if (direction == Direction.Right)
-        //    {
-        //        transform.Translate(Vector3.left * Time.deltaTime * (Mathf.Abs(GameData.Instance.RunnerVelocity.x) / scrollSpeedFactor));
-        //        if (Math.Truncate(transform.localPosition.x) <= -14f)
-        //        {
-        //            AddBgToRight();
-        //        }
-        //    }
-        //    else
-        //    {
-               
-        //        transform.Translate(Vector3.right * Time.deltaTime * (Mathf.Abs(GameData.Instance.RunnerVelocity.x) / scrollSpeedFactor));
-        //        if (Math.Truncate(transform.localPosition.x) >= 14f)
-        //        {
-        //            AddBgToLeft();
-        //        }
-        //    }
-
-
-        //}
-
-        //void AddBgToLeft()
-        //{
-        //    //Debug.Log("Adding to left");
-        //    GameObject temp = RightSprite;
-        //    RightSprite = CurrentSprite;
-        //    CurrentSprite = LeftSprite;
-        //    LeftSprite = temp;
-        //    LeftSprite.transform.localPosition = new Vector3(CurrentSprite.transform.localPosition.x - 1, 0, 0);
-        //    transform.localPosition = new Vector3(4.5f, transform.position.y, transform.position.z);
-        //}
-
-        //void AddBgToRight()
-        //{
-        //    //Debug.Log("Adding to right");
-        //    GameObject temp = LeftSprite;
-        //    LeftSprite = CurrentSprite;
-        //    CurrentSprite = RightSprite;
-        //    RightSprite = temp;
-        //    RightSprite.transform.localPosition = new Vector3(CurrentSprite.transform.localPosition.x + 1, 0, 0);
-        //    transform.localPosition = new Vector3(4.5f, transform.position.y, transform.position.z);
-        //}
-
-        private void ChangeMoveDirection(Direction direction)
-        {
-            this.direction = direction;
         }
 
     }
